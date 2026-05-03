096f95699f3aea41dc510a8b798ca9bb12598731
temandro <9128753@stud.kai.edu.ua>
2026-05-04 00:05:53 +0300

    add Homework

---
+++ Homework/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Developer: Perebyinis Artem");
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println("1");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                System.out.println("2");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+                System.out.println("3");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
